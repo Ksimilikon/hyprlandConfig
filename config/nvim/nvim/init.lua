ec5b27fb6b0dc3907bc86d41a30d7e4c0f662c00
ksimilikon <kirya.sozonov.mayl.ru@mail.ru>
2025-12-11 18:25:49 +0400

    update::waybar_niri_fuzzel

--- config/nvim/nvim/init.lua
+++
@@ -1,6 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-vim.cmd([[autocmd BufRead, BufNewFile *.slint set filetype=slint ]])
-vim.lsp.enable("slint_lsp")
-vim.lsp.enable("omnisharp")
-vim.cmd("colorscheme cyberdream")
